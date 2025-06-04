5f6b8a31e2d66c9cde4d8e108772a0467e0ba3db
robertsinfosec <490545+robertsinfosec@users.noreply.github.com>
2025-06-04 15:26:36 -0400

    Remove integrity attribute from Bootstrap CSS link in gatsby-ssr.ts

--- src/subosity-www/gatsby-ssr.ts
+++ src/subosity-www/gatsby-ssr.ts
@@ -7,7 +7,6 @@ export const onRenderBody: GatsbySSR["onRenderBody"] = ({ setHeadComponents }) =
       key: "bootstrap-css",
       rel: "stylesheet",
       href: "https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css",
-      integrity: "sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN",
       crossOrigin: "anonymous"
     }),
     React.createElement("style", {
